8ef66f77cfe2edccbf2c5c8c0cf65ff45ceb653f
Rishi Saimshu Reddy <rishireddy@uchicago.edu>
2024-03-09 06:21:22 -0600

    minor changes

--- db/20240305T183400-create_tables.sql
+++ db/20240305T183400-create_tables.sql
@@ -1,23 +1,17 @@
+create table reactions (
+  id INTEGER PRIMARY KEY,
+  emoji VARCHAR(100),
+  message_id INTEGER,
+  user_id INTEGER,
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(message_id) REFERENCES messages(id)
+);
+
 create table users (
   id INTEGER PRIMARY KEY,
   name VARCHAR(40) UNIQUE,
   password VARCHAR(40),
   auth_key VARCHAR(40)
-);
-
-create table channels (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(40) UNIQUE
-);
-
-create table messages (
-  id INTEGER PRIMARY KEY,
-  user_id INTEGER,
-  channel_id INTEGER,
-  body TEXT,
-  replies_to INTEGER,
-  FOREIGN KEY(user_id) REFERENCES users(id),
-  FOREIGN KEY(channel_id) REFERENCES channels(id)
 );
 
 create table unreads (
@@ -30,11 +24,17 @@ create table unreads (
   FOREIGN KEY(message_id) REFERENCES messages(id)
 );
 
-create table reaction (
+create table messages (
   id INTEGER PRIMARY KEY,
-  emoji VARCHAR(100),
-  message_id INTEGER,
   user_id INTEGER,
+  channel_id INTEGER,
+  body TEXT,
+  replies_to INTEGER,
   FOREIGN KEY(user_id) REFERENCES users(id),
-  FOREIGN KEY(message_id) REFERENCES messages(id)
+  FOREIGN KEY(channel_id) REFERENCES channels(id)
+);
+
+create table channels (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(40) UNIQUE
 );
